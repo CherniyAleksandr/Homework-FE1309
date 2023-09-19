d9756de562ba7bf9c21320e509a11cfac7fd2b03
CherniyAleksandr <132821035+CherniyAleksandr@users.noreply.github.com>
2023-09-19 12:32:56 +0200

    Add files via upload
    git commit

---
+++ Homework1309/script.js
@@ -0,0 +1,43 @@
+// const root = document.querySelector('#root');
+// const firstSection = document.createElement('section');
+// firstSection.classList.add('first-section');
+// root.appendChild(firstSection);
+
+// const heading = document.createElement('h1');
+// heading.textContent = "РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В МИРЕ";
+
+// const paragraph = document.createElement('p');
+// paragraph.textContent = "стадионы, газопроводы, мосты, дамбы";
+
+// const container = document.createElement('div');
+// container.classList.add('container');
+// firstSection.appendChild(container);
+
+// for (let i = 0; i < 4; i++) {
+//     const cardContainer = document.createElement('div');
+//     cardContainer.classList.add('card-container');
+
+//     const h3 = document.createElement('h3');
+//     h3.textContent = '26 лет на рынке';
+
+//     const h4 = document.createElement('h4');
+//     h4.textContent = '26 лет на рынке';
+
+//     const h5 = document.createElement('h5');
+//     h5.textContent = '26 лет на рынке';
+
+//     cardContainer.appendChild(h3);
+//     cardContainer.appendChild(h4);
+//     cardContainer.appendChild(h5);
+
+//     container.appendChild(cardContainer);
+// }
+
+
+
+
+
+
+
+
+
